f0c0cf0470d2c359b8e6f6f90a063c42e9a72b8f
madinash <madison.nash1@bobcats.gcsu.edu>
2023-11-29 14:16:57 -0500

    Final Version

--- app/functions.py
+++ app/functions.py
@@ -1,35 +1,60 @@
-from datetime import datetime
-import tkinter as tk
-from tkcalendar import Calendar
-from app.db import store_data_to_db
+# Import necessary modules and functions
+from datetime import datetime  # Importing datetime module to handle date and time
+import tkinter as tk  # Importing Tkinter for GUI
+from tkcalendar import Calendar  # Importing Calendar widget from tkcalendar module
+from app.db import store_data_to_db  # Importing store_data_to_db function from app/db module
 
+
+# Function to calculate date difference between two dates
 def calculate_date_difference(date1_str, date2_str, result_label):
     try:
+        # Convert date strings to datetime objects
         date1 = datetime.strptime(date1_str, "%Y-%m-%d")
         date2 = datetime.strptime(date2_str, "%Y-%m-%d")
+
+        # Calculate absolute difference in days between the dates
         date_diff = abs((date2 - date1).days)
+
+        # Update the result label with the calculated date difference
         result_label.config(text=f"Date difference: {date_diff} days")
     except Exception:
+        # Handle exceptions for invalid date format
         result_label.config(text="Invalid date format")
 
+
+# Function to store data to a text file
 def store_data_to_file(data):
     with open("dates.txt", "a") as file:
         file.write(data)
 
+
+# Function to calculate date difference and store dates
 def calculate_and_store_dates(date_picker1, date_picker2, result_label):
+    # Get selected dates from the date pickers
     date1_str = date_picker1.get_date()
     date2_str = date_picker2.get_date()
+
+    # Calculate date difference and update the result label
     calculate_date_difference(date1_str, date2_str, result_label)
+
+    # Format data for storage in a text file
     data = f"Date 1: {date1_str}\nDate 2: {date2_str}\nDate Difference: {result_label.cget('text')}\n\n"
+
+    # Store data to a text file
     store_data_to_file(data)
 
+
+# Function to display date differences in the window
 def display_differences(window):
+    # Create two date picker calendars
     date_picker1 = Calendar(window, date_pattern="yyyy-mm-dd", selectmode="day")
     date_picker2 = Calendar(window, date_pattern="yyyy-mm-dd", selectmode="day")
 
+    # Configure select background color for date pickers
     date_picker1.configure(selectbackground="hot pink")
     date_picker2.configure(selectbackground="hot pink")
 
+    # Pack date pickers into the window
     date_picker1.pack()
     date_picker2.pack()
 
@@ -37,6 +62,7 @@ def display_differences(window):
     result_label = tk.Label(window, text="")
     result_label.pack()
 
-    # Calculate and Store Button
-    calculate_button = tk.Button(window, text="Calculate and Store", command=lambda: calculate_and_store_dates(date_picker1, date_picker2, result_label))
+    # Create Calculate and Store Button
+    calculate_button = tk.Button(window, text="Calculate and Store",
+                                 command=lambda: calculate_and_store_dates(date_picker1, date_picker2, result_label))
     calculate_button.pack()
--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from tkinter import Tk
-from app.functions import display_differences
+# Import necessary modules and functions
+from tkinter import Tk  # Importing Tkinter for GUI
+from app.functions import display_differences  # Importing the display_differences function from app/functions module
 
+# Define the main function
 def main():
-    window = Tk()
-    window.title("Date Difference Calculator")
-    window.geometry("400x400")
+    window = Tk()  # Create the main window
+    window.title("Date Difference Calculator")  # Set the window title
+    window.geometry("400x400")  # Set the window size
 
-    # Call the display_differences function
+    # Call the display_differences function to calculate and display date differences in the window
     display_differences(window)
 
-    # Create a text file to store the date differences
+    # Create a text file named "dates.txt" to store the date differences (if it doesn't exist and overwrites if exists)
     file = open("dates.txt", "w")
-    file.close()
+    file.close()  # Close the file
 
-    window.mainloop()
+    window.mainloop()  # Run the main GUI event loop
 
+# Execute the main function if this script is run directly
 if __name__ == "__main__":
     main()
